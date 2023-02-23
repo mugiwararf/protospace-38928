6499eaf0a0f5c9621af90c7779a91a986ab1b130
mugiwararf <ayumi04190703@gmail.com>
2023-02-23 16:59:50 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,8 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @user = user.prototypes
     @name = user.name
+    @profile = user.profile
+    @occupation = user.occupation
+    @position = user.position
   end
 end
